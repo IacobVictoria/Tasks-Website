82120bf02de276c716dac469e2e25fc95421e5fd
IacobVictoria <161374115+IacobVictoria@users.noreply.github.com>
2024-08-08 23:59:22 +0300

    Implement reusable dropdown for form

--- tasks-app/src/task-folder/form-task/form-task.component.ts
+++ tasks-app/src/task-folder/form-task/form-task.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,24 +13,95 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Task } from '../task/task.interface';
+import { Member } from '../../_interfaces/detailed_member.interface';
+import { MemberService } from '../../_services/members-service.service';
+import { ActiveTaskService } from '../../_services/active_tasks.service';
+import { DropdownReusableComponent } from '../../_shared/dropdown-reusable/dropdown-reusable.component';
+import { SummaryMember } from '../../_interfaces/team-member.interface';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-form-task',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DropdownReusableComponent, CommonModule],
   templateUrl: './form-task.component.html',
   styleUrl: './form-task.component.css',
 })
-export class FormTaskComponent {
+export class FormTaskComponent implements OnInit {
   // sa se deschida form ul asta si daca am de adaugat si daca am de editat
   //daca e de editat sa imi apara datele deja acolo
   formGroupTask!: FormGroup;
+  @Input() task!: Task | null; // input pt a primi date de la componenta parinte
+  @Output() formSubmitted = new EventEmitter<Task>(); // trimite catre parinte
+  members: SummaryMember[] = [];
+  private taskService = inject(ActiveTaskService);
+  private memberService = inject(MemberService);
+
   constructor(private fb: FormBuilder) {
     this.formGroupTask = this.fb.group({
       title: ['', Validators.required],
       description: ['', Validators.required],
+      userId: ['', Validators.required],
     });
   }
 
-  onSubmit(): void {}
+  ngOnInit(): void {
+    
+    // If task data is provided (for editing), pre-fill the form
+    if (this.task) {
+      this.formGroupTask.patchValue({
+        title: this.task.title,
+        description: this.task.description,
+        userId: this.task.userId, // Pre-fill userId if available
+      });
+    }
+
+    this.memberService.getServices().subscribe({
+      next: (members) => {
+        this.members = members;
+        console.log(this.members); // Verifică dacă membrii sunt corect încărcați
+      },
+      error: (error) => {
+        console.error('Error fetching members:', error);
+      }
+    });
+    
+    
+  }
+
+  generateRandomId(): string {
+    return 'id-' + Math.random().toString(36).substring(2,9);
+  }
+
+  onSubmit(): void {
+    if (this.formGroupTask.valid) {
+      const formData: Task = {
+        ...this.formGroupTask.value,
+        id: this.task ? this.task.id : this.generateRandomId(), // Retain task ID for editing
+      };
+
+      
+      this.taskService.addTask(formData).subscribe({
+        next: (response) => {
+          console.log('Task added successfully:', response);
+          
+          this.formGroupTask.reset();
+        },
+        error: (error) => {
+          console.error('Error adding task:', error);
+        }
+      });
+    
+    }
+  }
+
+  onMemberSelect(event: Member) {
+    if (event && event.id) {
+      this.formGroupTask.patchValue({
+        userId: event.id,
+      });
+    }
+  }
 }
--- tasks-app/src/task-folder/task/task.component.ts
+++ tasks-app/src/task-folder/task/task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Task } from './task.interface';
 import { ActiveTask } from '../../_interfaces/task_active.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task',
@@ -11,11 +12,13 @@ import { ActiveTask } from '../../_interfaces/task_active.interface';
 })
 export class TaskComponent {
   @Input() task!: ActiveTask;
+  private router = inject(Router);
   @Output() edit = new EventEmitter<string>();
   @Output() delete = new EventEmitter<string>();
 
   editTask() {
     this.edit.emit(this.task.id);
+    this.router.navigate([`/edit-form`])
   }
 
   deleteTask() {
